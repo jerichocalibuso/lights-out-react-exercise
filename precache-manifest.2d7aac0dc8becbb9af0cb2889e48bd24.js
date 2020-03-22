d57f68d744043ea3293a2e59bcb1c5148bdd17c4
jerichocalibuso <jerichocalibuso@gmail.com>
2020-03-22 11:50:10 +0800

    Updates

---
+++ precache-manifest.2d7aac0dc8becbb9af0cb2889e48bd24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "353f7d16f6015497c20b",
+    "url": "/lights-out-react-exercise/static/css/main.5bd20b6d.chunk.css"
+  },
+  {
+    "revision": "353f7d16f6015497c20b",
+    "url": "/lights-out-react-exercise/static/js/main.353f7d16.chunk.js"
+  },
+  {
+    "revision": "45ea534042495dbb2393",
+    "url": "/lights-out-react-exercise/static/js/1.45ea5340.chunk.js"
+  },
+  {
+    "revision": "c6624cfa6724725cf51a",
+    "url": "/lights-out-react-exercise/static/js/runtime~main.c6624cfa.js"
+  },
+  {
+    "revision": "2a8168b3100208d4e2deb4ee662bf7a8",
+    "url": "/lights-out-react-exercise/index.html"
+  }
+];
